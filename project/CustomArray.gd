0212a060b6182e19bcfad087cfbb471c96c4ba9e
the-alex-g <alex.gestwicki@gmail.com>
2022-03-14 11:50:51 -0400

    Make text scrolling work

---
+++ project/CustomArray.gd
@@ -0,0 +1,46 @@
+class_name CustomArray
+extends Node
+# class description (one line)
+# extended class description
+
+# signals
+
+# enums
+
+# constants
+
+# exported variables
+
+# public variables
+
+# private variables
+var _array := []
+var _max_value := 0
+
+# onready variables
+
+# METHOD ORDER:
+# 1. built-in methods, starting with _init and _ready in that order
+# 2. public methods
+# 3. private methods
+
+func setup(max_value:int)->void:
+	_max_value = max_value
+
+
+func get_at(index:int):
+	if index < _array.size():
+		return _array[index]
+	else:
+		return " "
+
+
+func add(value:String)->void:
+	var transient_array := []
+	transient_array.append(value)
+	transient_array.append_array(_array)
+	_array = transient_array
+	
+	var array_length := _array.size()
+	if array_length > _max_value:
+		_array.remove(array_length-1)
--- project/Main.gd
+++ project/Main.gd
@@ -3,18 +3,37 @@ extends Control
 const SCANCODE_ALPHABET := {32:" ", 65:"a", 66:"b", 67:"c", 68:"d", 69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l", 77:"m", 78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v", 87:"w", 88:"x", 89:"y", 90:"z"}
 const ENTER := 16777221
 const BACKSPACE := 16777220
+const FOREGROUND_SEQUENCE := [
+	-1, 0, "n",
+	-1, 0, "n",
+	-1, 0, 0, "n",
+	0, 0, 0, 0, "n",
+	0, 0, 0, 0, "n",
+	0, 0, 0, 0, "n",
+	0, 0, 0, 0, "n",
+	0, 0, 0, 0, "n",
+	0, 0, 0, 0, "n",
+	0, 0, 0, 0, "n",
+]
 
 var _word_being_typed := ""
 var _words_entered := {}
 var _scrolling_string := ""
+var _foo := CustomArray.new()
+var _character_index : int
+var _character_counter := 0
 
 onready var _text_display = $TextDisplay as Label
 onready var _word_list = $WordList as RichTextLabel
+onready var _foreground = $CityContainer/Foreground as Label
+
+
+func _ready()->void:
+	_foo.setup(32)
 
 
 func _input(event:InputEvent)->void:
 	if event is InputEventKey:
-		#event = event as InputEventKey
 		if event.is_pressed():
 			var scancode = event.scancode
 			if SCANCODE_ALPHABET.keys().has(scancode):
@@ -35,7 +54,9 @@ func _submit_word()->void:
 	else:
 		_words_entered[_word_being_typed] = 1
 	_update_wordlist()
-	_scrolling_string += " " + _word_being_typed
+	
+	# adds word at start of _scrolling_string
+	_scrolling_string = _word_being_typed.capitalize() + _scrolling_string
 	_word_being_typed = ""
 	_text_display.text = _word_being_typed
 
@@ -49,3 +70,32 @@ func _update_wordlist()->void:
 		if times_entered > 1:
 			_word_list.text += " x" + str(times_entered)
 		_word_list.text += "\n"
+
+
+func _on_AdvanceStringTimer_timeout()->void:
+	var string_length := _scrolling_string.length()
+	if string_length == 0:
+		return
+	
+	_character_index = string_length - _character_counter
+	_character_counter += 1
+	if _character_counter >= string_length:
+		_character_counter %= string_length
+	var character = _scrolling_string[_character_index-1]
+	_foo.add(character)
+	
+	_draw_field()
+
+
+func _draw_field()->void:
+	var index := 0
+	_foreground.text = ""
+	for x in FOREGROUND_SEQUENCE:
+		match x:
+			-1:
+				_foreground.text += " "
+			0:
+				_foreground.text += _foo.get_at(index)
+				index += 1
+			"n":
+				_foreground.text += "\n"
