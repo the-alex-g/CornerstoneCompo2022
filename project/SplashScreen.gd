2aa17456a5d07d92eb3ae2966c2ddb6bcee6b1cc
the-alex-g <alex.gestwicki@gmail.com>
2022-03-30 19:15:07 -0400

    Remove audio from splashscreen

--- project/SplashScreen.gd
+++ project/SplashScreen.gd
@@ -2,9 +2,10 @@ extends Control
 
 
 func _on_Button_pressed()->void:
-	$AudioStreamPlayer.play()
+	#$AudioStreamPlayer.play()
+	get_tree().change_scene("res://Main.tscn")
 
 
 func _on_AudioStreamPlayer_finished()->void:
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://Main.tscn")
+	pass
